721823c8587794bca23bde460c001fb8fcd810d8
Kevin Geiss <kevin@desertsol.com>
2005-12-21 22:01:05 -0700

    try australia timezone
    committer: Kevin Geiss <kevin@quasar.localdomain>

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 int main()
 {
-  tz date( "US/Eastern" );
+  tz date( "Australia/Southern" );
 
   struct tm t = date.local_time();
 
   cout << asctime(&t);
+  cout << date.dst_active() << endl;
 
 
   return 0;
