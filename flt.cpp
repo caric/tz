2fab5a6c054b95c5b3857f6007911b12feab8ba7
Kevin Geiss <kevin.geiss@ticketmaster.com>
2013-06-24 23:36:51 -0700

    clean up and add duration of each flight in wall clock time

--- flt.cpp
+++ flt.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #include <tz.h>
 
 
-void convert( tz &there, int year, int mon, int mday, int hour, int min )
+void convert( const char*const label, tz &there, int year, int mon, int mday, int hour, int min )
 {
   struct tm there_tm = there.local_time();
   there_tm.tm_isdst = -1;
@@ -15,42 +15,32 @@ void convert( tz &there, int year, int mon, int mday, int hour, int min )
   there_tm.tm_hour = hour;
   there_tm.tm_min = min;
   there_tm.tm_sec = 0;
+  //there_tm.tm_zone = 0;
   there.set_local_time( there_tm );
+
+  cout << label << endl;
   cout << there.tz_name() << ": " << there << endl;
-  struct tm local = there.convert_from_there_to_here();
+
+  tz here(there);
+  struct tm local = here.convert_from_there_to_here();
   const char* const reference = "America/Phoenix";
   tz ref( reference );
   cout << "here will be: " << asctime(&local);
-  cout << " hours from " << reference << ": " << setw(5) << (ref.time_diff( there )/3600.0) << "\n" << endl;
+  cout << " time difference (hours) from " << reference << ": " << setw(5) << (ref.time_diff( here )/3600.0) << "\n" << endl;
 }
 
-void tester(
+void calculate_flight(
     const char* const start_tz, int start_year, int start_mon, int start_mday, int start_hour, int start_min,
     const char* const end_tz, int end_year, int end_mon, int end_mday, int end_hour, int end_min,
     const char* const local_time_zone
 ) {
   tz start( start_tz );
-  convert( start, start_year, start_mon, start_mday, start_hour, start_min );
+  convert( "takeoff", start, start_year, start_mon, start_mday, start_hour, start_min );
 
   tz end( end_tz );
-  convert( end, end_year, end_mon, end_mday, end_hour, end_min );
+  convert( "land", end, end_year, end_mon, end_mday, end_hour, end_min );
 
-  //cout << start_tz << ": " << start << endl;
-  //start.set_tz_name( local_time_zone );
-  //cout << "\t" << start << endl;
-  //cout << end_tz << ": " << end << endl;
-  //end.set_tz_name( local_time_zone );
-  //cout << "\t" << end << endl;
-
-  //const int pad = 25;
-  //cout << setw(pad) << tz_str << ": ";
-  ////for ( int i = pad; i-strlen(tz_str) > 0; i-- ) cout << ' ';
-  //cout << date << '\n';
-  ////for ( int i = pad; i > 0; i-- ) cout << ' ';
-  //cout << setw(pad+2) << " " << "DST active: " << (date.dst_active()?"Yes":" No") << "; ";
-  //const char* const reference = "America/Phoenix";
-  //tz ref( reference );
-  //cout << " hours from " << reference << ": " << setw(5) << (ref.time_diff( date )/3600.0) << "\n";
+  cout << " hours from takeoff in " << start.tz_name() << " to landing in " << end.tz_name() << ": " << setw(5) << (float)(end.duration( start )/3600.0) << "\n" << endl;
 
 }
 
@@ -90,24 +80,17 @@ Requested Seats: 	23E, 24E
 
 Total Travel Time: 34 hrs 19 mins 
 */
-  tester( "Asia/Muscat", 2013, 7, 8, 23, 35,
+  calculate_flight( "Asia/Muscat", 2013, 7, 8, 23, 35,
           "Europe/Zurich", 2013, 7, 9, 6, 20,
           "America/Phoenix"
   );
-  tester( "Europe/Zurich", 2013, 7, 9, 12, 55,
+  calculate_flight( "Europe/Zurich", 2013, 7, 9, 12, 55,
           "America/Chicago", 2013, 7, 9, 15, 40,
           "America/Phoenix"
   );
-  //tester( "America/Los_Angeles" );
-  //tester( "America/Chicago" );
-  //tester( "America/New_York" );
-  //tester( "Europe/London" );
-  //tester( "Europe/Paris" );
-  //tester( "Asia/Shanghai" );
-  //tester( "Australia/Sydney" );
-  //tester( "Asia/Calcutta" );
-  //tester( "GMT" );
-  //tester( "UTC" );
-
+  calculate_flight( "America/Chicago", 2013, 7, 9, 21, 5,
+          "America/Phoenix", 2013, 7, 9, 22, 54,
+          "America/Phoenix"
+  );
   return 0;
 }
