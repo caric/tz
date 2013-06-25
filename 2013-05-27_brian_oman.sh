a99498b4e4086c22e77a2bcf70145adda388c48f
Kevin Geiss <kevin.geiss@ticketmaster.com>
2013-06-25 10:32:16 -0700

    add brian trip to oman

--- 2013-05-27_brian_oman.sh
+++ 2013-05-27_brian_oman.sh
@@ -1,5 +1,74 @@
 #!/bin/bash
 
+# Mon, May 27, 2013
+# Phoenix Sky Harbor International Airport, (PHX) to Muscat, (MCT)
+#
+# Flight: 		United Flight 502    (on Airbus A320-100/200)
+# Depart: 		11:55 AM, Phoenix, AZ (PHX)
+# Arrive: 		05:15 PM, Chicago, IL (ORD)
+#
+# Requested Seats: 	32B, 32A 
+#
+# 1 Stop - change planes in Chicago, (ORD)
+# Connection Time:  2   hrs    10 mins 
+#
+#
+# Flight: 		United Flight 7681    operated by Swiss Intl Air Lines  (on Airbus A330-300)
+# Depart: 		07:25 PM, Chicago, IL (ORD)
+# Arrive: 		11:00 AM, Zurich, Switzerland (ZRH)
+#
+#
+#
+# 1 Stop - change planes in Zurich, (ZRH)
+# Connection Time:  1  hr    45 mins 
+#
+#
+# Flight: 		United Flight 7686    operated by Swiss Intl Air Lines  (on Airbus A330-300)
+# Depart: 		12:45 PM, Zurich, Switzerland (ZRH)
+# Arrive: 		10:35 PM, Next day Muscat, Oman (MCT)
+#
+#
+# 1 Stop - change planes Dubai, United Arab Emirates (DXB) 
+#
+#
+# Total Travel Time: 23 hrs 40 mins 
+# ______________________________________________________
+# 
+echo ===== fly from phoenix to chicago ======
+  flt \
+          "takeoff" "America/Phoenix" 2013 5 27 11 55 \
+          "land" "America/Chicago" 2013 5 27 17 15 \
+          "America/Phoenix"
+
+echo ===== wait in chicago ======
+  flt \
+          "land" "America/Chicago" 2013 5 27 17 15 \
+          "takeoff" "America/Chicago" 2013 5 27 19 25 \
+          "America/Phoenix"
+
+echo ==== fly from chicago to zurich =====
+  flt \
+          "takeoff" "America/Chicago" 2013 5 27 19 25 \
+          "land" "Europe/Zurich" 2013 5 28 11 00 \
+          "America/Phoenix"
+
+echo ==== wait in zurich ====
+  flt \
+          "land" "Europe/Zurich" 2013 5 28 11 00 \
+          "takeoff" "Europe/Zurich" 2013 5 28 12 45 \
+          "America/Phoenix"
+
+echo ==== fly from zurich to oman ====
+  flt \
+          "takeoff" "Europe/Zurich" 2013 5 28 12 45 \
+          "land" "Asia/Muscat" 2013 5 28 22 35 \
+          "America/Phoenix"
+
+
+# 
+# For your boarding pass, use reference code B7KSW6 for online or airport checkin.
+# 
+# 
 #  Mon, Jul 8, 2013
 #Muscat, (MCT) to Phoenix Sky Harbor International Airport, (PHX)
 #
